0d65e1d76680e152fe2072bb59cd53b02978e841
Renato-DEV <Renato-DEV@Renato>
2026-08-20 15:04:53 -0300

    * Correção do parser do XML * Melhoria no danfse

--- src/main/java/com/nfsenacional/xml/NfseXmlParser.java
+++ src/main/java/com/nfsenacional/xml/NfseXmlParser.java
@@ -1,15 +1,42 @@
 package com.nfsenacional.xml;
 
+import com.nfsenacional.dto.nfse.CodigoServicoData;
+import com.nfsenacional.dto.nfse.DescontoData;
+import com.nfsenacional.dto.nfse.DpsData;
 import com.nfsenacional.dto.nfse.EmitenteData;
+import com.nfsenacional.dto.nfse.EnderecoData;
 import com.nfsenacional.dto.nfse.EnderecoEmitenteData;
+import com.nfsenacional.dto.nfse.EnderecoExteriorData;
 import com.nfsenacional.dto.nfse.IbscbsNfseData;
+import com.nfsenacional.dto.nfse.InfDpsData;
 import com.nfsenacional.dto.nfse.InfEventoData;
 import com.nfsenacional.dto.nfse.InfNfseData;
+import com.nfsenacional.dto.nfse.LocalPrestacaoData;
 import com.nfsenacional.dto.nfse.NfseData;
+import com.nfsenacional.dto.nfse.PrestadorData;
+import com.nfsenacional.dto.nfse.RegimeTributarioData;
+import com.nfsenacional.dto.nfse.ServicoData;
+import com.nfsenacional.dto.nfse.TomadorData;
+import com.nfsenacional.dto.nfse.TributacaoData;
+import com.nfsenacional.dto.nfse.ValorServicoPrestadoData;
+import com.nfsenacional.dto.nfse.ValoresData;
 import com.nfsenacional.dto.nfse.ValoresNfseData;
 import com.nfsenacional.enums.AmbienteGerador;
 import com.nfsenacional.enums.CodigoStatus;
+import com.nfsenacional.enums.CstPisCofins;
+import com.nfsenacional.enums.EmitenteDPS;
+import com.nfsenacional.enums.MotivoEmissaoTomadorIntermediario;
+import com.nfsenacional.enums.MotivoNaoNif;
+import com.nfsenacional.enums.OpcaoSimplesNacional;
 import com.nfsenacional.enums.ProcessoEmissao;
+import com.nfsenacional.enums.RegimeApuracaoSN;
+import com.nfsenacional.enums.RegimeEspecialTributacao;
+import com.nfsenacional.enums.TipoAmbiente;
+import com.nfsenacional.enums.TipoImunidade;
+import com.nfsenacional.enums.TipoRetencaoIssqn;
+import com.nfsenacional.enums.TipoRetencaoPisCofins;
+import com.nfsenacional.enums.TipoSuspensao;
+import com.nfsenacional.enums.TributacaoIssqn;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -101,6 +128,11 @@ public class NfseXmlParser {
 
         // dps: não reconstituído nesta versão (ver javadoc da classe).
 
+        Element dpsEl = firstChild(el, "DPS");
+        if (dpsEl != null) {
+            b.dps(parseDps(dpsEl));
+        }
+
         Element emitEl = firstChild(el, "emit");
         if (emitEl != null) {
             b.emitente(parseEmitente(emitEl));
@@ -114,6 +146,259 @@ public class NfseXmlParser {
         Element ibscbsEl = firstChild(el, "IBSCBS");
         if (ibscbsEl != null) {
             b.ibscbs(parseIbscbsNfse(ibscbsEl));
+        }
+
+        return b.build();
+    }
+
+    private DpsData parseDps(Element el) {
+        Element infDpsEl = firstChild(el, "infDPS");
+        return DpsData.builder()
+                .versao(el.getAttribute("versao"))
+                .infDps(infDpsEl != null ? parseInfDps(infDpsEl) : null)
+                .build();
+    }
+
+    private InfDpsData parseInfDps(Element el) {
+        InfDpsData.InfDpsDataBuilder b = InfDpsData.builder()
+                .id(el.getAttribute("Id"))
+                .dataEmissao(text(el, "dhEmi"))
+                .versaoAplicativo(text(el, "verAplic"))
+                .serie(text(el, "serie"))
+                .numeroDps(text(el, "nDPS"))
+                .dataCompetencia(text(el, "dCompet"))
+                .codigoLocalEmissao(text(el, "cLocEmi"))
+                .chaveNfseRejeitada(text(el, "chNFSeRej"));
+
+        String tpAmb = text(el, "tpAmb");
+        if (tpAmb != null) {
+            b.tipoAmbiente(TipoAmbiente.fromCodigo(tpAmb));
+        }
+        String tpEmit = text(el, "tpEmit");
+        if (tpEmit != null) {
+            b.tipoEmitente(EmitenteDPS.fromCodigo(tpEmit));
+        }
+        String cMotivoEmisTI = text(el, "cMotivoEmisTI");
+        if (cMotivoEmisTI != null) {
+            b.motivoEmissaoTomadorIntermediario(MotivoEmissaoTomadorIntermediario.fromCodigo(cMotivoEmisTI));
+        }
+
+        Element prestEl = firstChild(el, "prest");
+        if (prestEl != null) {
+            b.prestador(parsePrestador(prestEl));
+        }
+        Element tomaEl = firstChild(el, "toma");
+        if (tomaEl != null) {
+            b.tomador(parseTomador(tomaEl));
+        }
+        Element servEl = firstChild(el, "serv");
+        if (servEl != null) {
+            b.servico(parseServico(servEl));
+        }
+        Element valoresEl = firstChild(el, "valores");
+        if (valoresEl != null) {
+            b.valores(parseValoresDps(valoresEl));
+        }
+
+        return b.build();
+    }
+
+    private PrestadorData parsePrestador(Element el) {
+        PrestadorData.PrestadorDataBuilder b = PrestadorData.builder()
+                .cnpj(text(el, "CNPJ"))
+                .cpf(text(el, "CPF"))
+                .nif(text(el, "NIF"))
+                .codigoNaoNif(text(el, "cNaoNIF"))
+                .caepf(text(el, "CAEPF"))
+                .inscricaoMunicipal(text(el, "IM"))
+                .nome(text(el, "xNome"))
+                .telefone(text(el, "fone"))
+                .email(text(el, "email"));
+
+        Element endEl = firstChild(el, "end");
+        if (endEl != null) {
+            b.endereco(parseEndereco(endEl));
+        }
+        Element regTribEl = firstChild(el, "regTrib");
+        if (regTribEl != null) {
+            b.regimeTributario(parseRegimeTributario(regTribEl));
+        }
+
+        return b.build();
+    }
+
+    private TomadorData parseTomador(Element el) {
+        TomadorData.TomadorDataBuilder b = TomadorData.builder()
+                .cnpj(text(el, "CNPJ"))
+                .cpf(text(el, "CPF"))
+                .nif(text(el, "NIF"))
+                .caepf(text(el, "CAEPF"))
+                .inscricaoMunicipal(text(el, "IM"))
+                .nome(text(el, "xNome"))
+                .telefone(text(el, "fone"))
+                .email(text(el, "email"));
+
+        String cNaoNif = text(el, "cNaoNIF");
+        if (cNaoNif != null) {
+            b.codigoNaoNif(MotivoNaoNif.fromCodigo(cNaoNif));
+        }
+        Element endEl = firstChild(el, "end");
+        if (endEl != null) {
+            b.endereco(parseEndereco(endEl));
+        }
+
+        return b.build();
+    }
+
+    private EnderecoData parseEndereco(Element el) {
+        Element endNacEl = firstChild(el, "endNac");
+        Element endExtEl = firstChild(el, "endExt");
+        String cep = text(endNacEl, "CEP");
+        if (cep == null) {
+            cep = text(el, "CEP");
+        }
+
+        EnderecoData.EnderecoDataBuilder b = EnderecoData.builder()
+                .codigoMunicipio(text(endNacEl, "cMun"))
+                .cep(cep)
+                .logradouro(text(el, "xLgr"))
+                .numero(text(el, "nro"))
+                .bairro(text(el, "xBairro"))
+                .complemento(text(el, "xCpl"));
+
+        if (endExtEl != null) {
+            b.enderecoExterior(EnderecoExteriorData.builder()
+                    .codigoPais(text(endExtEl, "cPais"))
+                    .codigoEnderecamentoPostal(text(endExtEl, "cEndPost"))
+                    .cidade(text(endExtEl, "xCidade"))
+                    .estadoProvinciaRegiao(text(endExtEl, "xEstProvReg"))
+                    .build());
+        }
+
+        return b.build();
+    }
+
+    private RegimeTributarioData parseRegimeTributario(Element el) {
+        RegimeTributarioData.RegimeTributarioDataBuilder b = RegimeTributarioData.builder();
+        String opSimpNac = text(el, "opSimpNac");
+        if (opSimpNac != null) {
+            b.opcaoSimplesNacional(OpcaoSimplesNacional.fromCodigo(opSimpNac));
+        }
+        String regApTribSN = text(el, "regApTribSN");
+        if (regApTribSN != null) {
+            b.regimeApuracaoTributosSn(RegimeApuracaoSN.fromCodigo(regApTribSN));
+        }
+        String regEspTrib = text(el, "regEspTrib");
+        if (regEspTrib != null) {
+            b.regimeEspecialTributacao(RegimeEspecialTributacao.fromCodigo(regEspTrib));
+        }
+        return b.build();
+    }
+
+    private ServicoData parseServico(Element el) {
+        ServicoData.ServicoDataBuilder b = ServicoData.builder();
+
+        Element locPrestEl = firstChild(el, "locPrest");
+        if (locPrestEl != null) {
+            b.localPrestacao(LocalPrestacaoData.builder()
+                    .codigoLocalPrestacao(text(locPrestEl, "cLocPrestacao"))
+                    .codigoPaisPrestacao(text(locPrestEl, "cPaisPrestacao"))
+                    .build());
+        }
+        Element cServEl = firstChild(el, "cServ");
+        if (cServEl != null) {
+            b.codigoServico(CodigoServicoData.builder()
+                    .codigoTributacaoNacional(text(cServEl, "cTribNac"))
+                    .codigoTributacaoMunicipal(text(cServEl, "cTribMun"))
+                    .descricaoServico(text(cServEl, "xDescServ"))
+                    .codigoNbs(text(cServEl, "cNBS"))
+                    .codigoInternoContribuinte(text(cServEl, "cIntContrib"))
+                    .build());
+        }
+
+        return b.build();
+    }
+
+    private ValoresData parseValoresDps(Element el) {
+        ValoresData.ValoresDataBuilder b = ValoresData.builder();
+
+        Element vServPrestEl = firstChild(el, "vServPrest");
+        if (vServPrestEl != null) {
+            b.valorServicoPrestado(ValorServicoPrestadoData.builder()
+                    .valorRecebido(doubleOrNull(text(vServPrestEl, "vReceb")))
+                    .valorServico(doubleOrNull(text(vServPrestEl, "vServ")))
+                    .build());
+        }
+        Element vDescEl = firstChild(el, "vDescCondIncond");
+        if (vDescEl != null) {
+            b.desconto(DescontoData.builder()
+                    .valorDescontoIncondicionado(doubleOrNull(text(vDescEl, "vDescIncond")))
+                    .valorDescontoCondicionado(doubleOrNull(text(vDescEl, "vDescCond")))
+                    .build());
+        }
+        Element tribEl = firstChild(el, "trib");
+        if (tribEl != null) {
+            b.tributacao(parseTributacao(tribEl));
+        }
+
+        return b.build();
+    }
+
+    private TributacaoData parseTributacao(Element el) {
+        TributacaoData.TributacaoDataBuilder b = TributacaoData.builder();
+
+        Element tribMunEl = firstChild(el, "tribMun");
+        if (tribMunEl != null) {
+            String tribISSQN = text(tribMunEl, "tribISSQN");
+            if (tribISSQN != null) {
+                b.tributacaoIssqn(TributacaoIssqn.fromCodigo(Integer.parseInt(tribISSQN)));
+            }
+            String tpImunidade = text(tribMunEl, "tpImunidade");
+            if (tpImunidade != null) {
+                b.tipoImunidade(TipoImunidade.fromCodigo(Integer.parseInt(tpImunidade)));
+            }
+            String tpRetISSQN = text(tribMunEl, "tpRetISSQN");
+            if (tpRetISSQN != null) {
+                b.tipoRetencaoIssqn(TipoRetencaoIssqn.fromCodigo(Integer.parseInt(tpRetISSQN)));
+            }
+            b.aliquota(doubleOrNull(text(tribMunEl, "pAliq")));
+
+            Element exigSuspEl = firstChild(tribMunEl, "exigSusp");
+            if (exigSuspEl != null) {
+                String tpSusp = text(exigSuspEl, "tpSusp");
+                if (tpSusp != null) {
+                    b.tipoSuspensao(TipoSuspensao.fromCodigo(Integer.parseInt(tpSusp)));
+                }
+                b.numeroProcessoSuspensao(text(exigSuspEl, "nProcesso"));
+            }
+        }
+
+        Element tribFedEl = firstChild(el, "tribFed");
+        if (tribFedEl != null) {
+            Element piscofinsEl = firstChild(tribFedEl, "piscofins");
+            if (piscofinsEl != null) {
+                String cst = text(piscofinsEl, "CST");
+                if (cst != null) {
+                    b.cstPisCofins(CstPisCofins.fromCodigo(cst));
+                }
+                b.baseCalculoPisCofins(doubleOrNull(text(piscofinsEl, "vBCPisCofins")))
+                        .aliquotaPis(doubleOrNull(text(piscofinsEl, "pAliqPis")))
+                        .aliquotaCofins(doubleOrNull(text(piscofinsEl, "pAliqCofins")))
+                        .valorPis(doubleOrNull(text(piscofinsEl, "vPis")))
+                        .valorCofins(doubleOrNull(text(piscofinsEl, "vCofins")));
+
+                String tpRetPisCofins = text(piscofinsEl, "tpRetPisCofins");
+                if (tpRetPisCofins != null) {
+                    b.tipoRetencaoPisCofins(TipoRetencaoPisCofins.fromCodigo(Integer.parseInt(tpRetPisCofins)));
+                }
+            }
+            b.valorRetidoIrrf(doubleOrNull(text(tribFedEl, "vRetIRRF")))
+                    .valorRetidoCsll(doubleOrNull(text(tribFedEl, "vRetCSLL")));
+        }
+
+        Element totTribEl = firstChild(el, "totTrib");
+        if (totTribEl != null) {
+            b.percentualTotalTributosSN(doubleOrNull(text(totTribEl, "pTotTribSN")));
         }
 
         return b.build();
